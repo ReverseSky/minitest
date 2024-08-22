73481055af23a3857d791098adce392d9571b26e
ReverseSky <gregoryrobledo99@gmail.com>
2024-08-22 12:06:30 +0200

    ex fini

--- cursus42/cpp/cpp01/ex04/main.cpp
+++ cursus42/cpp/cpp01/ex04/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: grobledo <grobledo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: greg <greg@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 16:38:54 by grobledo          #+#    #+#             */
-/*   Updated: 2024/08/21 17:02:21 by grobledo         ###   ########.fr       */
+/*   Updated: 2024/08/22 12:05:08 by greg             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,44 @@ int main(int argc, char **argv)
 {
 	if (argc != 4)
 	{
-		std::cout << "error comment must be : ./replace filename string1 string2" << std::endl;
+		std::cout << "error comment must be : ./replace filename <string1> <string2>" << std::endl;
 	}
-	std::string	filename = argv[1];
-	std::string	s1 = argv[2];
-	std::string	s2 = argv[3];
+	if (argc == 4)
+	{
+		std::string	filename(argv[1]);
+		std::string	s1 = argv[2];
+		std::string	s2 = argv[3];
+		int	c;
 
-	
+		std::ifstream	ifs(filename.c_str());
+		c = ifs.peek();
+		if (c == EOF)
+			std::cout << "error file is empty" << std::endl;
+		else
+		{
+			std::ofstream ofs(filename.append(".replace").data());
+			while (ifs.good() && ofs.good())
+			{
+				std::string line;
+				std::size_t found;
+				std::getline(ifs, line);
+				found = line.find(s1, 0);
+				while (found != std::string::npos)
+				{
+					line.erase(found, s1.length());
+					line.insert(found, s2);
+					found = line.find(s1, found);
+				}
+				ofs << line;
+				if (ifs.eof())
+					break;
+				ofs << std::endl;
+			}
+			ifs.close();
+			ofs.close();
+		}
+	}
+	else
+			std::cout << "Error can not open file " << std::endl;
 }
+
