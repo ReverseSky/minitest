b0c6fb39f3d494e630bdce62d458b65d80ffa595
Gregory Robledo <grobledo@c2r7p5.42perpignan.fr>
2024-09-30 16:26:08 +0200

    cub3d

--- cursus42/cub3d/cub3d.h
+++ cursus42/cub3d/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: grobledo <grobledo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 15:45:59 by grobledo          #+#    #+#             */
-/*   Updated: 2024/09/30 16:20:03 by grobledo         ###   ########.fr       */
+/*   Updated: 2024/09/30 16:25:57 by grobledo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,7 @@ https://github.com/iciamyplant/Cub3d-Linux
 https://www.youtube.com/watch?v=gID_FKfncZI&themeRefresh=1
 
 https://lodev.org/cgtutor/raycasting.html
+
+https://www.youtube.com/watch?v=js7HW65MmNw&list=PL0H9-oZl_QOHM34HvD3DiGmwmj5X7GvTW
 
 #endif
